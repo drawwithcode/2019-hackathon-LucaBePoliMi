e7f3fe3d0c0cf22bc8cdd317bffd640b79432811
LucaBePoliMi <luca.b97@gmail.com>
2019-11-27 12:52:43 +0100

    Code is commented

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 
 var bumperAudio;
+// Variable to be used to measure volume
 var resizer;
 
+// Variables for switch x and y
 var sx;
 var sy;
 
@@ -24,30 +26,43 @@ function setup() {
 }
 
 var volume = 0;
+
+// Variables for switches images
 var switchSpento;
 var switchAcceso;
+
+// Variable for opacity, it allows the logo not be shown at the beginning
 var opacity = 255;
 
+// Variable for toggling the switches
 var imgOpacity = 255;
 
 function draw() {
 
-
+// Measuring the volume
   volume = resizer.getLevel();
-  volume = map(volume, 0, 1, 0, (width*height) / 1000) // To make measures responsive
+  // Mapping it
+  // To make measures responsive width*height will be used
+  volume = map(volume, 0, 1, 0, (width*height) / 1000)
+
 
 background(0, 0, 0);
 
+// When the audio plays
+
 if(bumperAudio.isPlaying() == true) {
 
+//The logo is shown
 opacity = 0;
+// the background is grey
 background(23, 23, 23);
-
+// The switch toggles
 imgOpacity = 0;
 
 }
 
 else if (bumperAudio.isPlaying() == false) {
+  // The switch is on "off"
   imgOpacity = 255;
 }
 
@@ -74,7 +89,7 @@ pop();
 
 }
 
-
+// Background for the logo
 for(e = 0; e < 7; e += 0.17) {
 push();
 stroke(255);
@@ -83,17 +98,21 @@ noFill();
 pop();
 }
 
+// Logo image
 push();
 imageMode(CENTER);
 var logo = image(tg1Img, width/2, height/2, tg1Img.width/50*(width*height) / 100000, tg1Img.height/50*(width*height) / 100000);
 pop();
 
+// Rectangle to cover the logo at the beginning
 fill(0, 0, 0, opacity);
 rect(0, 0, width, height);
 
+// Defining switches' x and y
 sx = 9/10 * width
 sy = 9/10 * height
 
+// Creating the switches' images
 switchAcceso = image(switchAccesoImg, sx, sy, switchAccesoImg.width/1000000*(width*height), switchAccesoImg.height/1000000*(width*height));
 push();
 tint(255, imgOpacity);
@@ -101,6 +120,7 @@ switchSpento = image(switchSpentoImg, sx, sy, switchSpentoImg.width/1000000*(wid
 pop();
   }
 
+// The audio plays and the switch toggles just when click is on switch
   function mouseClicked() {
 if (bumperAudio.isPlaying() == false) {
   if (mouseX > sx && mouseX < sx + switchSpentoImg.width/1000000*(width*height)
